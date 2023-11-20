bbd51f607e04ac5df7acbf70b80e92bd828042ae
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-11-20 23:52:58 +0400

    added nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindTypography from '@tailwindcss/typography'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss','@nuxthq/studio'],
   tailwindcss: {
     config: {
         plugins: [tailwindTypography],
