977a4e822ee6d9e3247dc87a26223717b1eb31eb
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-11-20 23:44:48 +0400

    added article list

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+      <ContentList path="/" v-slot="{ list }">
+        <div v-for="article in list" :key="article._path">
+          <a :href="article.slug">{{ article.title }}</a>
+        </div>
+      </ContentList>
+    </main>
+  </template>
