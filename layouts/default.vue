de29b9b3a214626ddfbb3b6b6acede332aa2bd02
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-01-02 18:56:42 +0400

    kick netlify

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,11 +9,3 @@
     <SiteFooter />
   </div>
 </template>
-
-<script setup>
-useHead({
-  script: [
-    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-  ],
-});
-</script>
