933d7ff1b9ed52dd1d423a3852abfeb0aaa04e9d
Sandeep Ramgolam <sandeep@ramgolam.com>
2024-01-02 17:39:02 +0400

    added netlify identity

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,3 +9,11 @@
     <SiteFooter />
   </div>
 </template>
+
+<script setup>
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+</script>
